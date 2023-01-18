6075d94ce85a8a13420451fc8f1c611676321410
Uttam Sawant <uttam.sawant@atos.net>
2023-01-18 18:32:43 +0530

    Initial Commits

---
+++ DemoWebApplication/DemoWebApplication/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DemoWebApplication.Startup))]
+namespace DemoWebApplication
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
